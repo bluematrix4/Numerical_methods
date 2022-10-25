4c25527d38b8181ca083adf6b240570d5065fd5f
Anwesh Mishra <96272066+bluematrix4@users.noreply.github.com>
2022-10-25 12:10:53 +0530

    Add files via upload

---
+++ Secant method.py
@@ -0,0 +1,13 @@
+from mpmath import *
+mp.dps=50
+x0= str(input("x0: "))
+x1= str(input("x1: "))
+x0,x1=mpf(x0),mpf(x1)
+
+f= lambda x: x**3 - 5*x -6   # INPUT YOUR FUNCTION HERE
+
+n=int(input("Iterations: "))
+for k in range(n):
+    y=mpf(str(x1-((x1-x0)*f(x1)/(f(x1)-f(x0)))))
+    print(y)
+    x0,x1=x1,y
